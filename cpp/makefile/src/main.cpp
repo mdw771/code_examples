feb3d6e2251164930fe06c5a44c5078916b8a2fd
Ming Du <ming.du101@gmail.com>
2023-02-26 15:48:13 -0500

    Makefile example

---
+++ cpp/makefile/src/main.cpp
@@ -0,0 +1,11 @@
+#include <main.hpp>
+#include <module1.hpp>
+
+using namespace std;
+
+int main()
+{
+    cout << "This is main." << endl;
+    worker1();
+    return 0;
+}
---
+++ cpp/makefile/src/module1.cpp
@@ -0,0 +1,9 @@
+#include <module1.hpp>
+
+using namespace std;
+
+int worker1()
+{
+    cout << "This is worker1." << endl;
+    return 0;
+}
